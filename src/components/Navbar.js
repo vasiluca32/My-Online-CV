6fefff5d1cf4fe7cbbe418c5b57e504aeb53a39b
vasiluca32 <vasilelucaluca@gmail.com>
2020-06-09 22:16:38 +0300

    09.06.2020 home render, first render with picture

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,14 @@
 import React from "react";
 import "./Navbar.css"
+import { Link } from "react-router-dom";
+
 
 export default function Navbar() {
     return (
         <div className="navbar">
-            <a href="#home">Home</a>
+            <Link className="navbar-brand" to="/Home">
+                Home
+            </Link>
             <a href="#contact">Skills & Objecives</a>
             <a href="#news">Trainings</a>
             <a href="#contact">Work history</a>
