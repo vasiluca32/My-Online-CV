85ad787e07d45b2c0a9e652a384ca7944394eb72
vasiluca32 <vasilelucaluca@gmail.com>
2020-06-29 18:36:15 +0300

    wallpaper fix

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,13 @@
 import React from "react";
 import "./Home.css";
+import Wallpaper from "../wallpaper-office.png"
 
 export default function Home() {
 
     return (
         <div className="home-container">
             <div className="home-wrapper">
-                <img className="wallpaper" src="https://i.dlpng.com/static/png/6622507_preview.png" alt="wallpaper"></img>
+                <img className="wallpaper" src={Wallpaper} alt="wallpaper"></img>
                 <div className="home-text">
                     <h1>WORK HARD IN SILENCE,<br></br>LET YOUR SUCCESS BE YOUR NOISE</h1>
                 </div>
