64dffdff8aa95f2b7a2dc1aebf1540980d751b2e
vasiluca32 <vasilelucaluca@gmail.com>
2020-07-15 01:01:00 +0300

    video done

---
+++ src/pages/Thank-you.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Thank-you.css";
+
+export default function Video() {
+
+    return (
+        <div className="video-wrap">
+
+            <p>video</p>
+
+            <iframe width="600" height="450" src="https://www.youtube.com/embed/wPmTp9up26w?controls=0">
+
+            </iframe>
+
+        </div>
+    )
+}
