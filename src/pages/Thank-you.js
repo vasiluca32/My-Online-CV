8271ff721a6e957e5d63b6185878bab981aa9c4f
vasiluca32 <vasilelucaluca@gmail.com>
2020-08-01 11:30:52 +0300

    final

--- src/pages/Thank-you.js
+++ src/pages/Thank-you.js
@@ -1,16 +1,17 @@
 import React from "react";
 import "./Thank-you.css";
+import clip from "../../src/video.mp4";
+import poster from "../../src/poster.jpg";
 
 export default function Video() {
 
     return (
         <div className="video-wrap">
+            <video className="frame" src={clip} type="video/mp4" poster={poster} controls>
 
-
-            <iframe className="frame" src="https://www.youtube.com/embed/wPmTp9up26w?controls=0">
-
-            </iframe>
-
+                Your browser does not support the video tag.
+            </video>
         </div>
+
     )
 }
