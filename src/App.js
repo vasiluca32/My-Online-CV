605defd64806d3287b58287537abcf23dc3740e4
vasiluca32 <vasilelucaluca@gmail.com>
2020-08-20 21:36:18 +0300

    git pages

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 import Header from "./components/Header"
 import Body from "./components/Body"
 import Footer from "./components/Footer"
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import Pannel from "./components/Side-pannel"
 
 function App() {
