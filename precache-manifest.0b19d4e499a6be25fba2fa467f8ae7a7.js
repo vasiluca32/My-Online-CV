bdadaf1c9c136c6fe09bbbb5d98f4858b4bdf499
vasiluca32 <vasilelucaluca@gmail.com>
2020-10-21 23:50:52 +0300

    Updates

---
+++ precache-manifest.0b19d4e499a6be25fba2fa467f8ae7a7.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0129d930b7d124fa9540abf23916d18",
+    "url": "/My-Online-CV/index.html"
+  },
+  {
+    "revision": "2e3d69367f70854d8ed8",
+    "url": "/My-Online-CV/static/css/2.95e8e5c3.chunk.css"
+  },
+  {
+    "revision": "9cceb10e1f9c7e969838",
+    "url": "/My-Online-CV/static/css/main.788db732.chunk.css"
+  },
+  {
+    "revision": "2e3d69367f70854d8ed8",
+    "url": "/My-Online-CV/static/js/2.73758615.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/My-Online-CV/static/js/2.73758615.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9cceb10e1f9c7e969838",
+    "url": "/My-Online-CV/static/js/main.29677640.chunk.js"
+  },
+  {
+    "revision": "af5d17b3b8099c568c74",
+    "url": "/My-Online-CV/static/js/runtime-main.6b9417e8.js"
+  },
+  {
+    "revision": "3879df5690ab01e8d6d253e3934427ce",
+    "url": "/My-Online-CV/static/media/cv-pic.3879df56.jpg"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/My-Online-CV/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/My-Online-CV/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/My-Online-CV/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/My-Online-CV/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/My-Online-CV/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "e44b59375c7f4e8bd8f2f1d39b234537",
+    "url": "/My-Online-CV/static/media/index.e44b5937.php"
+  },
+  {
+    "revision": "abe3713cece8cf4a4f6f229d0ac8d7bb",
+    "url": "/My-Online-CV/static/media/poster.abe3713c.jpg"
+  },
+  {
+    "revision": "800f5fb274be1da433b322d227f61279",
+    "url": "/My-Online-CV/static/media/video.800f5fb2.mp4"
+  },
+  {
+    "revision": "33ac839ab21cec9388ea6223549527ff",
+    "url": "/My-Online-CV/static/media/wallpaper-office.33ac839a.png"
+  }
+]);
