0c472c03da0ac17b2395843e8f0f0eccca3cc443
vasiluca32 <vasilelucaluca@gmail.com>
2020-11-29 22:54:37 +0200

    .

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const transporter = require('./config');
 const dotenv = require('dotenv');
 dotenv.config();
 const app = express();
+const port = 3000
 
 const buildPath = path.join(__dirname, '..', 'build');
 app.use(express.json());
@@ -51,6 +52,8 @@ app.post('/send', (req, res) => {
     }
 });
 
+
+
 app.listen(3030, () => {
-    console.log('server start on port 3030');
+    console.log(`Example app listening at http://localhost:${port}`);
 });
